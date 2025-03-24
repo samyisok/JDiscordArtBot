97862953d6bc5f7951d9480eca6c30ee9c34a46a
Sergey <magaster@gmail.com>
2025-03-25 01:04:33 +0300

    init

---
+++ src/main/java/ru/sarahbot/sarah/SarahApplication.java
@@ -0,0 +1,13 @@
+package ru.sarahbot.sarah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sarahbot/sarah/SarahApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sarahbot.sarah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
