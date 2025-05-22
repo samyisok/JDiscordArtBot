22600800570cb0e15433d4c30160604f053c9f75
Sergey <magaster@gmail.com>
2025-05-22 21:21:36 +0300

    kubeconfig

--- k8s/deploy.bat
+++ k8s/deploy.bat
@@ -2,13 +2,16 @@
 setlocal enabledelayedexpansion
 
 :: Load environment variables from .env file
-for /f "usebackq tokens=1,* delims==" %%a in ("..\\.env") do (
+for /f "usebackq tokens=1,* delims==" %%a in (".env") do (
     set "%%a=%%b"
 )
 
+echo Using kubeconfig: !KUBECONFIG!
+kubectl config current-context
+
 echo Building and pushing new container image...
-nerdctl build -t !REGISTRY_URL!/sarah-bot:latest ..
-nerdctl push !REGISTRY_URL!/sarah-bot:latest
+nerdctl.exe build --insecure-registry -t !REGISTRY_URL!/sarah-bot:latest ..
+nerdctl.exe push --insecure-registry !REGISTRY_URL!/sarah-bot:latest
 
 echo Deleting old pod...
 kubectl -n sarah-bot delete pod --all
